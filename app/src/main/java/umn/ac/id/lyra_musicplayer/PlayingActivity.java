e8e3bf17ada0f0a362969793ef60b803582837b7
Erika Putrim <eputrim@gmail.com>
2021-03-10 15:46:26 +0700

    Now Playing UI

---
+++ app/src/main/java/umn/ac/id/lyra_musicplayer/PlayingActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.lyra_musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing);
+    }
+}
